029eae0b4962c4faf03a9647136f822244862c16
shaojunda <shaojunda@gmail.com>
2020-09-17 16:57:40 +0800

    feat: support collect multiple cells

--- client/transaction.go
+++ client/transaction.go
@@ -31,9 +31,12 @@ var (
 	ErrNotAcpLock               = errors.New("address must acp address")
 	ErrUnknownToken             = errors.New("unknown token")
 	ErrNoneAcpCell              = errors.New("none acy cell")
+	ErrToAddrNoneAcpCell        = errors.New("toAddr none acy cell")
 	ErrInvalidTransferUdtAmount = errors.New("amount is invalid")
 	ErrInvalidFromAddress       = errors.New("from address must be a acp address")
 	ErrInvalidToAddress         = errors.New("to address must be a acp address")
+	ErrFromScriptMissMatch      = errors.New("fromAddr does not math from script")
+	ErrToScriptMissMatch        = errors.New("toAddr does not math to script")
 )
 
 type tokenInfo struct {
@@ -176,8 +179,8 @@ func buildCkbTransaction(fromAddr string, toAddr string, from *types.Script, to 
 	return tx, inputs, nil
 }
 
-func buildUdtTransactionNew(fromAddr string, toAddr string, from *types.Script, to *types.Script, amount string, tokenIdentifier string, client rpc.Client, config *config.Config) (*types.Transaction, []btx.Input, error) {
-	_, err:= validateAddresses(fromAddr, toAddr, config)
+func buildUdtTransaction(fromAddr string, toAddr string, from *types.Script, to *types.Script, amount string, tokenIdentifier string, client rpc.Client, config *config.Config) (*types.Transaction, []btx.Input, error) {
+	err := validateAddresses(fromAddr, toAddr, from, to, config)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -192,9 +195,6 @@ func buildUdtTransactionNew(fromAddr string, toAddr string, from *types.Script, 
 	}
 
 	inputs := make([]btx.Input, 0)
-	var toAcpCellOriginAmount *big.Int
-	var toAcpCellOriginCapacity uint64
-	var fromAcpCellOriginCapacity uint64
 
 	tx := &types.Transaction{
 		Version:    0,
@@ -202,26 +202,302 @@ func buildUdtTransactionNew(fromAddr string, toAddr string, from *types.Script, 
 		CellDeps:   []*types.CellDep{},
 	}
 	buildCellDeps(tx, config)
+	toAcpCellOriginAmount, toAcpCellOriginCapacity, collectedFromAcpCellAmount, fromAcpCellOriginCapacity, extraCapacity, err := buildInputs(fromAddr, toAddr, from, to, client, config, tInfo, tx, inputs, transferUdtAmount)
+	if err != nil {
+		return nil, nil, err
+	}
+	finalToAcpCellAmountBytes, finalFromAcpCellAmountBytes := buildOutputs(tInfo, tx, from, to, config, toAcpCellOriginCapacity, toAcpCellOriginAmount, transferUdtAmount, fromAcpCellOriginCapacity, collectedFromAcpCellAmount, extraCapacity)
+	buildOutputsData(tx, finalToAcpCellAmountBytes, finalFromAcpCellAmountBytes)
+	buildWitnesses(tx)
+	err = handleTxFee(tx, client, inputs, fromAddr, from, fromAcpCellOriginCapacity, extraCapacity)
+	if err != nil {
+		return nil, nil, err
+	}
 
-	return nil, nil, nil
+	return tx, inputs, nil
 }
 
-func validateAddresses(fromAddr string, toAddr string, config *config.Config) (bool, error) {
+func handleTxFee(tx *types.Transaction, client rpc.Client, inputs []btx.Input, fromAddr string, from *types.Script, fromAcpCellOriginCapacity uint64, extraCapacity uint64) error {
+	fee, err := transaction.CalculateTransactionFee(tx, FeeRate)
+	if err != nil {
+		return err
+	}
+	fee += uint64(len(tx.Witnesses)-1) * 8
+
+	var needCollectInputForFee bool
+	if fromAcpCellOriginCapacity-fee < UdtCapacity {
+		if extraCapacity > 0 {
+			extraOutput := tx.Outputs[len(tx.Outputs)-1]
+			extraOutput.Capacity = extraCapacity - fee
+		} else {
+			needCollectInputForFee = true
+		}
+	} else {
+		tx.Outputs[1].Capacity = fromAcpCellOriginCapacity - fee
+	}
+
+	if needCollectInputForFee {
+		// set change output
+		changeOutput := &types.CellOutput{
+			Capacity: 0,
+			Lock:     from,
+		}
+		tx.Outputs = append(tx.Outputs, changeOutput)
+		tx.OutputsData = append(tx.OutputsData, []byte{})
+		var cursor string
+		searchKey := &indexer.SearchKey{
+			Script:     from,
+			ScriptType: indexer.ScriptTypeLock,
+		}
+		var hasEnoughCapacity bool
+		liveCells, err := client.GetCells(context.Background(), searchKey, indexer.SearchOrderAsc, MaxInput, cursor)
+		if err != nil {
+			return err
+		}
+		for _, cell := range liveCells.Objects {
+			if cell.Output.Type == nil && len(cell.OutputData) == 0 {
+				tx.Inputs = append(tx.Inputs, &types.CellInput{
+					Since:          0,
+					PreviousOutput: cell.OutPoint,
+				})
+				inputs = append(inputs, btx.Input{
+					Value:   fmt.Sprintf("%d", cell.Output.Capacity),
+					Address: fromAddr,
+				})
+				tx.Witnesses = append(tx.Witnesses, []byte{})
+				fee, err := transaction.CalculateTransactionFee(tx, FeeRate)
+				if err != nil {
+					return err
+				}
+				fee += uint64(len(tx.Witnesses)-1) * 8
+				changeOutput.Capacity = cell.Output.Capacity - fee
+				hasEnoughCapacity = true
+				break
+			}
+		}
+		if !hasEnoughCapacity {
+			return ErrInsufficientCkbBalance
+		}
+	}
+	return nil
+}
+
+func buildWitnesses(tx *types.Transaction) {
+	emptyWitness, _ := transaction.EmptyWitnessArg.Serialize()
+	for i := 0; i < len(tx.Inputs); i++ {
+		if i == 1 {
+			tx.Witnesses = append(tx.Witnesses, emptyWitness)
+		} else {
+			tx.Witnesses = append(tx.Witnesses, []byte{})
+		}
+	}
+}
+
+func buildOutputsData(tx *types.Transaction, finalToAcpCellAmountBytes []byte, finalFromAcpCellAmountBytes []byte) {
+	tx.OutputsData = append(tx.OutputsData, finalToAcpCellAmountBytes, finalFromAcpCellAmountBytes)
+	if len(tx.Outputs) > 2 {
+		for i := 2; i < len(tx.Outputs); i++ {
+			tx.OutputsData = append(tx.OutputsData, []byte{})
+		}
+	}
+}
+
+func buildOutputs(tInfo tokenInfo, tx *types.Transaction, from *types.Script, to *types.Script, config *config.Config, toAcpCellOriginCapacity uint64, toAcpCellOriginAmount *big.Int, transferUdtAmount *big.Int, fromAcpCellOriginCapacity uint64, collectedFromAcpCellAmount *big.Int, extraCapacity uint64) (finalToAcpCellAmountBytes []byte, finalFromAcpCellAmountBytes []byte) {
+	udtArgs, _ := hex.DecodeString(tInfo.TokenIdentifier[2:])
+	// set to acp cell to outputs
+	tx.Outputs = append(tx.Outputs, &types.CellOutput{
+		Capacity: toAcpCellOriginCapacity,
+		Lock:     to,
+		Type: &types.Script{
+			CodeHash: types.HexToHash(config.UDT.Script.CodeHash),
+			HashType: types.ScriptHashType(config.UDT.Script.HashType),
+			Args:     udtArgs,
+		},
+	})
+	fmt.Println(toAcpCellOriginAmount.String())
+	fmt.Println(collectedFromAcpCellAmount.String())
+	finalToAcpCellAmount := big.NewInt(0).Add(toAcpCellOriginAmount, transferUdtAmount)
+	finalToAcpCellAmountBytes = generateSudtAmount(finalToAcpCellAmount)
+	// set from acp cell to outputs
+	tx.Outputs = append(tx.Outputs, &types.CellOutput{
+		Capacity: fromAcpCellOriginCapacity,
+		Lock:     from,
+		Type: &types.Script{
+			CodeHash: types.HexToHash(config.UDT.Script.CodeHash),
+			HashType: types.ScriptHashType(config.UDT.Script.HashType),
+			Args:     udtArgs,
+		},
+	})
+
+	finalFromAcpCellAmount := big.NewInt(0).Sub(collectedFromAcpCellAmount, transferUdtAmount)
+	finalFromAcpCellAmountBytes = generateSudtAmount(finalFromAcpCellAmount)
+	fmt.Println(finalFromAcpCellAmount.String())
+
+	if extraCapacity > 0 {
+		tx.Outputs = append(tx.Outputs, &types.CellOutput{
+			Capacity: extraCapacity,
+			Lock:     from,
+		})
+	}
+	return
+}
+
+func generateSudtAmount(finalToAcpCellAmount *big.Int) []byte {
+	b := finalToAcpCellAmount.Bytes()
+	for i := 0; i < len(b)/2; i++ {
+		b[i], b[len(b)-i-1] = b[len(b)-i-1], b[i]
+	}
+	if len(b) < 16 {
+		for i := len(b); i < 16; i++ {
+			b = append(b, 0)
+		}
+	}
+	return b
+}
+
+func buildInputs(fromAddr string, toAddr string, from *types.Script, to *types.Script, client rpc.Client, config *config.Config, tInfo tokenInfo, tx *types.Transaction, inputs []btx.Input, transferUdtAmount *big.Int) (toAcpCellOriginAmount *big.Int, toAcpCellOriginCapacity uint64, collectedFromAcpCellAmount *big.Int, fromAcpCellOriginCapacity uint64, extraCapacity uint64, err error) {
+	toSearchKey := &indexer.SearchKey{
+		Script:     to,
+		ScriptType: indexer.ScriptTypeLock,
+	}
+	// collect toAddr first acp cell
+	var toLiveCells *indexer.LiveCells
+	toLiveCells, err = client.GetCells(context.Background(), toSearchKey, indexer.SearchOrderAsc, MaxInput, "")
+	if err != nil {
+		return
+	}
+	for _, cell := range toLiveCells.Objects {
+		if cell.Output.Type != nil && cell.Output.Type.CodeHash.String() == config.UDT.Script.CodeHash {
+			args := "0x" + hex.EncodeToString(cell.Output.Type.Args)
+			if tInfo.TokenIdentifier == args {
+				toAcpCellOriginCapacity = cell.Output.Capacity
+				tx.Inputs = append(tx.Inputs, &types.CellInput{
+					Since:          0,
+					PreviousOutput: cell.OutPoint,
+				})
+				toAcpCellOriginAmount = sudtAmount(cell.OutputData)
+				inputs = append(inputs, btx.Input{
+					Value:           toAcpCellOriginAmount.String(),
+					Address:         toAddr,
+					TokenCode:       tInfo.TokenCode,
+					TokenIdentifier: tInfo.TokenIdentifier,
+					TokenDecimal:    tInfo.TokenDecimal,
+				})
+				break
+			}
+		}
+	}
+	if len(tx.Inputs) == 0 {
+		err = ErrToAddrNoneAcpCell
+		return
+	}
+
+	fromSearchKey := &indexer.SearchKey{
+		Script:     from,
+		ScriptType: indexer.ScriptTypeLock,
+	}
+
+	var cursor string
+	var hasEnoughUdt bool
+	collectedFromAcpCellAmount = big.NewInt(0)
+	var fromLiveCells *indexer.LiveCells
+	var collectedFromAcpCellCount int
+	// collect fromAddr acp cells
+	for {
+		fromLiveCells, err = client.GetCells(context.Background(), fromSearchKey, indexer.SearchOrderAsc, MaxInput, cursor)
+		if err != nil {
+			return
+		}
+		for _, cell := range fromLiveCells.Objects {
+			if cell.Output.Type != nil && cell.Output.Type.CodeHash.String() == config.UDT.Script.CodeHash {
+				args := "0x" + hex.EncodeToString(cell.Output.Type.Args)
+				if tInfo.TokenIdentifier == args {
+					udtAmount := sudtAmount(cell.OutputData)
+					collectedFromAcpCellAmount.Add(collectedFromAcpCellAmount, udtAmount)
+					if collectedFromAcpCellCount == 0 {
+						fromAcpCellOriginCapacity = cell.Output.Capacity
+					} else {
+						extraCapacity += cell.Output.Capacity
+					}
+					collectedFromAcpCellCount++
+					tx.Inputs = append(tx.Inputs, &types.CellInput{
+						Since:          0,
+						PreviousOutput: cell.OutPoint,
+					})
+					inputs = append(inputs, btx.Input{
+						Value:           udtAmount.String(),
+						Address:         fromAddr,
+						TokenCode:       tInfo.TokenCode,
+						TokenIdentifier: tInfo.TokenIdentifier,
+						TokenDecimal:    tInfo.TokenDecimal,
+					})
+					if collectedFromAcpCellAmount.Cmp(transferUdtAmount) >= 0 {
+						hasEnoughUdt = true
+						break
+					}
+				}
+			}
+		}
+		if hasEnoughUdt || len(fromLiveCells.Objects) < 1000 || fromLiveCells.LastCursor == "" {
+			break
+		}
+		cursor = fromLiveCells.LastCursor
+	}
+
+	if !hasEnoughUdt {
+		err = ErrInsufficientSudtBalance
+		return
+	}
+	return
+}
+
+func sudtAmount(outputData []byte) *big.Int {
+	b := outputData[0:16]
+	for i := 0; i < len(b)/2; i++ {
+		b[i], b[len(b)-i-1] = b[len(b)-i-1], b[i]
+	}
+
+	return big.NewInt(0).SetBytes(b)
+}
+
+func validateAddresses(fromAddr string, toAddr string, from *types.Script, to *types.Script, config *config.Config) error {
 	fromParsedAddr, err := address.Parse(fromAddr)
 	if err != nil {
-		return false, err
+		return err
 	}
 	if fromParsedAddr.Script.CodeHash.String() != config.ACP.Script.CodeHash {
-		return false, ErrInvalidFromAddress
+		return ErrInvalidFromAddress
 	}
 	toParsedAddr, err := address.Parse(toAddr)
 	if err != nil {
-		return false, err
+		return err
 	}
 	if toParsedAddr.Script.CodeHash.String() != config.ACP.Script.CodeHash {
-		return false, err
+		return err
 	}
-	return true, nil
+	fromLockHash, err := fromParsedAddr.Script.Hash()
+	if err != nil {
+		return err
+	}
+	fromPLockHash, err := from.Hash()
+	if err != nil {
+		return err
+	}
+	if fromLockHash != fromPLockHash {
+		return ErrFromScriptMissMatch
+	}
+	toLockHash, err := toParsedAddr.Script.Hash()
+	if err != nil {
+		return err
+	}
+	toPLockHash, err := to.Hash()
+	if err != nil {
+		return err
+	}
+	if toLockHash != toPLockHash {
+		return ErrToScriptMissMatch
+	}
+	return nil
 }
 
 func buildCellDeps(tx *types.Transaction, config *config.Config) {
@@ -254,252 +530,6 @@ func generateTokenInfo(config *config.Config, tokenIdentifier string) (tInfo tok
 		return tokenInfo{}, ErrUnknownToken
 	}
 	return
-}
-
-func buildUdtTransaction(fromAddr string, toAddr string, from *types.Script, to *types.Script, amount string, tokenIdentifier string, client rpc.Client, config *config.Config) (*types.Transaction, []btx.Input, error) {
-	var uuid string
-	var decimals int
-	var symbol string
-	for key, token := range config.UDT.Tokens {
-		if key == tokenIdentifier {
-			uuid = key
-			symbol = token.Symbol
-			decimals = token.Decimal
-			break
-		}
-	}
-	if uuid == "" {
-		return nil, nil, ErrUnknownToken
-	}
-
-	inputs := make([]btx.Input, 0)
-	var total *big.Int
-	total, _ = big.NewInt(0).SetString(amount, 10)
-	var origin *big.Int
-	var originToCKB uint64
-	var originFromCKB uint64
-
-	scripts, err := utils.NewSystemScripts(client)
-	if err != nil {
-		return nil, nil, err
-	}
-	tx := transaction.NewSecp256k1SingleSigTx(scripts)
-	tx.CellDeps[0] = &types.CellDep{
-		OutPoint: &types.OutPoint{
-			TxHash: types.HexToHash(config.ACP.Deps[0].TxHash),
-			Index:  config.ACP.Deps[0].Index,
-		},
-		DepType: types.DepType(config.ACP.Deps[0].DepType),
-	}
-	tx.CellDeps = append(tx.CellDeps, &types.CellDep{
-		OutPoint: &types.OutPoint{
-			TxHash: types.HexToHash(config.UDT.Deps[0].TxHash),
-			Index:  config.UDT.Deps[0].Index,
-		},
-		DepType: types.DepType(config.UDT.Deps[0].DepType),
-	})
-
-	if to.CodeHash.String() == config.ACP.Script.CodeHash {
-		searchKey := &indexer.SearchKey{
-			Script:     to,
-			ScriptType: indexer.ScriptTypeLock,
-		}
-		liveCells, err := client.GetCells(context.Background(), searchKey, indexer.SearchOrderAsc, MaxInput, "")
-		if err != nil {
-			return nil, nil, err
-		}
-		for _, cell := range liveCells.Objects {
-			if cell.Output.Type != nil && cell.Output.Type.CodeHash.String() == config.UDT.Script.CodeHash {
-				args := "0x" + hex.EncodeToString(cell.Output.Type.Args)
-				if uuid == args {
-					originToCKB = cell.Output.Capacity
-					tx.Inputs = append(tx.Inputs, &types.CellInput{
-						Since:          0,
-						PreviousOutput: cell.OutPoint,
-					})
-					b := cell.OutputData
-					for i := 0; i < len(b)/2; i++ {
-						b[i], b[len(b)-i-1] = b[len(b)-i-1], b[i]
-					}
-					origin = big.NewInt(0).SetBytes(b)
-
-					inputs = append(inputs, btx.Input{
-						Value:           origin.String(),
-						Address:         toAddr,
-						TokenCode:       symbol,
-						TokenIdentifier: tokenIdentifier,
-						TokenDecimal:    decimals,
-					})
-					tx.Witnesses = append(tx.Witnesses, []byte{})
-					break
-				}
-			}
-		}
-		if len(tx.Inputs) == 0 {
-			return nil, nil, ErrNoneAcpCell
-		}
-	}
-
-	toNormal := false
-	if len(tx.Inputs) == 0 {
-		origin = big.NewInt(0)
-		originToCKB = UdtCapacity
-		toNormal = true
-	}
-
-	searchKey := &indexer.SearchKey{
-		Script:     from,
-		ScriptType: indexer.ScriptTypeLock,
-	}
-	liveCells, err := client.GetCells(context.Background(), searchKey, indexer.SearchOrderAsc, MaxInput, "")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	stopCkb := false
-	stopUdt := false
-	var ckbBalance uint64
-	var udtBalance *big.Int
-	for _, cell := range liveCells.Objects {
-		if toNormal && cell.Output.Type == nil && len(cell.OutputData) == 0 {
-			if (cell.Output.Capacity <= CkbCapacity*2) || (!toNormal && cell.Output.Capacity <= CkbCapacity) {
-				return nil, nil, ErrInsufficientCkbBalance
-			}
-		}
-		if !stopUdt && cell.Output.Type != nil && cell.Output.Type.CodeHash.String() == config.UDT.Script.CodeHash {
-			args := "0x" + hex.EncodeToString(cell.Output.Type.Args)
-			if uuid == args {
-				b := cell.OutputData
-				for i := 0; i < len(b)/2; i++ {
-					b[i], b[len(b)-i-1] = b[len(b)-i-1], b[i]
-				}
-				udtBalance = big.NewInt(0).SetBytes(b)
-				originFromCKB = cell.Output.Capacity
-
-				if udtBalance.Cmp(total) < 0 {
-					continue
-				}
-
-				tx.Inputs = append(tx.Inputs, &types.CellInput{
-					Since:          0,
-					PreviousOutput: cell.OutPoint,
-				})
-				inputs = append(inputs, btx.Input{
-					Value:           udtBalance.String(),
-					Address:         fromAddr,
-					TokenCode:       symbol,
-					TokenIdentifier: tokenIdentifier,
-					TokenDecimal:    decimals,
-				})
-				tx.Witnesses = append(tx.Witnesses, []byte{})
-				stopUdt = true
-			}
-		}
-		if stopUdt {
-			break
-		}
-	}
-
-	if !stopUdt {
-		return nil, nil, ErrInsufficientSudtBalance
-	}
-	emptyWitness, _ := transaction.EmptyWitnessArg.Serialize()
-	if toNormal {
-		tx.Witnesses[0] = emptyWitness
-	} else {
-		tx.Witnesses[1] = emptyWitness
-	}
-
-	udtArgs, _ := hex.DecodeString(uuid[2:])
-	tx.Outputs = append(tx.Outputs, &types.CellOutput{
-		Capacity: originToCKB,
-		Lock:     to,
-		Type: &types.Script{
-			CodeHash: types.HexToHash(config.UDT.Script.CodeHash),
-			HashType: types.ScriptHashType(config.UDT.Script.HashType),
-			Args:     udtArgs,
-		},
-	})
-	origin = big.NewInt(0).Add(total, origin)
-	b := origin.Bytes()
-	for i := 0; i < len(b)/2; i++ {
-		b[i], b[len(b)-i-1] = b[len(b)-i-1], b[i]
-	}
-	if len(b) < 16 {
-		for i := len(b); i < 16; i++ {
-			b = append(b, 0)
-		}
-	}
-	tx.OutputsData = append(tx.OutputsData, b)
-
-	tx.Outputs = append(tx.Outputs, &types.CellOutput{
-		Capacity: originFromCKB,
-		Lock:     from,
-		Type: &types.Script{
-			CodeHash: types.HexToHash(config.UDT.Script.CodeHash),
-			HashType: types.ScriptHashType(config.UDT.Script.HashType),
-			Args:     udtArgs,
-		},
-	})
-	b = big.NewInt(0).Sub(udtBalance, total).Bytes()
-	for i := 0; i < len(b)/2; i++ {
-		b[i], b[len(b)-i-1] = b[len(b)-i-1], b[i]
-	}
-	if len(b) < 16 {
-		for i := len(b); i < 16; i++ {
-			b = append(b, 0)
-		}
-	}
-	tx.OutputsData = append(tx.OutputsData, b)
-
-	fee, err := transaction.CalculateTransactionFee(tx, FeeRate)
-	fee += 8
-	if err != nil {
-		return nil, nil, err
-	}
-	originFromCKBBalance := tx.Outputs[1].Capacity
-	if originFromCKBBalance-fee < UdtCapacity {
-		for _, cell := range liveCells.Objects {
-			if !stopCkb && cell.Output.Type == nil && len(cell.OutputData) == 0 {
-				if (toNormal && cell.Output.Capacity <= CkbCapacity*2) || (!toNormal && cell.Output.Capacity <= CkbCapacity) {
-					return nil, nil, ErrInsufficientCkbBalance
-				}
-				tx.Inputs = append(tx.Inputs, &types.CellInput{
-					Since:          0,
-					PreviousOutput: cell.OutPoint,
-				})
-				tx.Witnesses = append(tx.Witnesses, []byte{})
-				ckbBalance = cell.Output.Capacity
-				stopCkb = true
-			}
-			if stopCkb {
-				break
-			}
-		}
-
-		if !stopCkb {
-			return nil, nil, ErrInsufficientCkbBalance
-		}
-
-		tx.Outputs = append(tx.Outputs, &types.CellOutput{
-			Capacity: 0,
-			Lock:     from,
-		})
-		tx.OutputsData = append(tx.OutputsData, []byte{})
-		fee, err := transaction.CalculateTransactionFee(tx, FeeRate)
-		fee += 8
-		if err != nil {
-			return nil, nil, err
-		}
-		tx.Outputs[2].Capacity = ckbBalance - fee
-		if toNormal {
-			tx.Outputs[2].Capacity = ckbBalance - UdtCapacity - fee
-		}
-	} else {
-		tx.Outputs[1].Capacity = originFromCKBBalance - fee
-	}
-
-	return tx, inputs, nil
 }
 
 func BuildEmptyTransaction(from string, to string, client rpc.Client, config *config.Config) (*types.Transaction, []btx.Input, error) {
