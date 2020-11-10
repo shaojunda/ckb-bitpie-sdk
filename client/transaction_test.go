24e18d7a23ffea427fb072132cf4d3d163741370
shaojunda <shaojunda@gmail.com>
2020-11-10 21:39:31 +0800

    test: reject transfer to old acp address

--- client/transaction_test.go
+++ client/transaction_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestErrorNotSupportTransferToOldAcpAddress(t *testing.T) {
+func TestErrorNotSupportTransferFromOldAcpAddress(t *testing.T) {
 	conf, err := config.Load("../config-example.yaml")
 	if err != nil {
 		t.Error(err)
@@ -14,6 +14,19 @@ func TestErrorNotSupportTransferToOldAcpAddress(t *testing.T) {
 	toAddr := "ckt1qyqt705jmfy3r7jlvg88k87j0sksmhgduazq7x5l8k"
 	_, _, err = BuildNormalTransaction(fromAddr, toAddr, "1000", "", nil, conf)
 	if err != ErrorNotSupportTransferFromOldAcpAddress {
+		t.Errorf("should return error %v", err)
+	}
+}
+
+func TestErrorNotSupportTransferToOldAcpAddress(t *testing.T) {
+	conf, err := config.Load("../config-example.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+	fromAddr := "ckt1qyqt705jmfy3r7jlvg88k87j0sksmhgduazq7x5l8k"
+	toAddr := "ckt1qjr2r35c0f9vhcdgslx2fjwa9tylevr5qka7mfgmscd33wlhfykyhazydxllj3dzvalznz08fs6dugc5mwkhxgdnkqu"
+	_, _, err = BuildNormalTransaction(fromAddr, toAddr, "1000", "", nil, conf)
+	if err != ErrorNotSupportTransferToOldAcpAddress {
 		t.Errorf("should return error %v", err)
 	}
 }
